f261a7c31bb2dce5b1bac3dcf8d6729e755eaa51
sharathts <ts.sharath18@gmail.com>
2017-01-25 03:51:42 -0800

    With POS tagging

--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -2,6 +2,7 @@ from preprocess import *
 from feature_extraction import *
 from feature_selection import *
 from kmeans import *
+import CMUTweetTagger
 
 def main():
     filename = "clinton-50k.csv"
@@ -19,15 +20,28 @@ def main():
     #Text preprocessing - stopwords, stemming, lowercase
     preprocessed_data_sample = tokenize_and_stopwords(data_sample)
     preprocessed_data_sample = stemmer(preprocessed_data_sample)
-    print preprocessed_data_sample[0:5]
-
     #use CMU tagger and remove NNP and NNPS
     
+    print "CMU tagger"
+    all_tags = CMUTweetTagger.runtagger_parse(preprocessed_data_sample)
+    for i in range(len(all_tags)):
+        for tag in all_tags[i]:
+#            print tag[1]
+            if tag[1] == 'NNP' or tag[1] == 'NNPS':
+#                print "yes"
+#                print preprocessed_data_sample[i]
+                preprocessed_data_sample[i] = preprocessed_data_sample[i].replace(tag[0], '')
+#                print preprocessed_data_sample[i]
+#                print all_tags[i]
 #    -----To be done------
 #------------------------------------------     
-
+    
+    #TFIDF features without text processing
+    [data_tfidf, vectorizer, no_features] = vectorize(data_sample, TFIDF) #Feature set 3
+    
     #TFIDF features with text processing
-    [data_tfidf_plus, vectorizer, no_features] = vectorize(data_sample, TFIDF) #Feature set 3
+    [data_tfidf_plus, vectorizer, no_features] = vectorize(preprocessed_data_sample, TFIDF) #Feature set 4
+    
 #    #Unigram features with text processing
 #    [data_uni_plus, vectorizer, no_features] = vectorize(data_sample, UNI)
    
