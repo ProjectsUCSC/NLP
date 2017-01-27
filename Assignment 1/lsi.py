9095395a84f6e07a9d920daff17f00374136bc26
shubhangi-tandon <shubhi194@gmail.com>
2017-01-26 19:39:43 -0800

    Final code changes for Assignment 1

--- Assignment 1/lsi.py
+++ Assignment 1/lsi.py
@@ -2,11 +2,11 @@ from sklearn.decomposition import TruncatedSVD
 from sklearn.random_projection import sparse_random_matrix
 
 def lsa(data):
-    
+
     #X = sparse_random_matrix(100, 100, density=0.01, random_state=42)
     print "Running LSA"
-    svd = TruncatedSVD(n_components=100, n_iter=7, random_state=42)
-    return svd.fit_transform(data) 
-#    print(svd.explained_variance_ratio_) 
+    svd = TruncatedSVD(n_components=1000, n_iter=9, random_state=42)
+    return svd.fit_transform(data)
+#    print(svd.explained_variance_ratio_)
 #    print(svd.explained_variance_ratio_.sum())
 #    return data
