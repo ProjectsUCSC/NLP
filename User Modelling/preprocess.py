377b7bdff640194123af6bc55cf4f7902d2f2b6b
sharathts <ts.sharath18@gmail.com>
2017-02-21 20:55:24 -0800

    more data

--- User Modelling/preprocess.py
+++ User Modelling/preprocess.py
@@ -17,24 +17,22 @@ def readData(filename1, filename2):
     req_attributes = ['tweet_id', 'topic', 'sentiment', 'tweet', 'user_id', 'followers_count', 'statuses_count', 'description', 'friends_count', 'location']
     path = cwd + "/data/" + filename1;
     tweet_df = pd.read_csv(path, sep='\t');
+    
+    tweet_df = tweet_df.drop_duplicates(['tweet_id'])
+#    print Counter(list(tweet_df["tweet_id"])).most_common(50)
 #    user_df["tweet_id"] = int(user_df["tweet_id"])
 
     path = cwd + "/data/" + filename2;
     user_df = pd.read_csv(path, sep='\t')
     user_df = user_df.dropna(subset=['user_id'])
     
-#    user_df = user_df[np.isfinite(user_df['user_id'])]
-#    user_df = user_df[!(math.isnan(user_df["user_id"]))]
-#    tweet_df["tweet_id"] = int(tweet_df["tweet_id"])
+#    print Counter(list(user_df["user_id"]))
+#    print tweet_df["tweet"][np.array(tweet_df['tweet_id'])]# == 641616155619258368]
 
-#    user_df['tweet_id'] = user_df['tweet_id'].astype('int')
-#    tweet_df['tweet_id'] = tweet_df['tweet_id'].astype('int')
-
-     
 #    data = pd.merge(tweet_df, user_df, on="tweet_id")
-    tweet_ids = np.array((user_df["tweet_id"]))
-    tweet_ids_extra = np.array((tweet_df["tweet_id"]))
-    length = len(tweet_df)
+#    tweet_ids = np.array((user_df["tweet_id"]))
+#    tweet_ids_extra = np.array((tweet_df["tweet_id"]))
+#    length = len(tweet_df)
     
 #    print "selecting"
 #    indices = np.array([i for i in range(length) if tweet_ids_extra[i] in tweet_ids])
@@ -49,6 +47,10 @@ def readData(filename1, filename2):
 #    k = user_df.groupby(['user_id'])
 #    print k
     data = data.dropna(subset=['user_id'])
+    print len(user_df), len(tweet_df), len(data)
+    
+    print "From user data\n", Counter(list(data["user_id"])).most_common(50)
+#    data = data.drop_duplicates(['tweet_id'])
     return data[req_attributes]
 
 
@@ -122,6 +124,7 @@ def stemmer(preprocessed_data_sample):
 def preprocess(filename1, filename2):
 	#filename = "Homework2_data.csv"
 	df = readData(filename1, filename2)
+	print "from joined data\n", Counter(list(df["user_id"])).most_common(50)
 #	df["user_id"] = df[df["tweet_id"] != 'Nan']
 #	print df["user_id"]
 #	df['text'] = df['Tweet_text'].apply(cleanhtml).apply(cleanUrl).apply(removeMention);#.apply(stemmer);
@@ -129,9 +132,12 @@ def preprocess(filename1, filename2):
 ##	df['text'] = stemmer(df["text"])
 #	df['text'] = tokenize_and_stopwords(df['text'])
 #	return df
-	print Counter(list(df["user_id"]))
-	print list(df) 
+#	print df["tweets"][df[]]
+#	print Counter(list(df["tweet_id"]))
+#	print list(df)
+
+ 
 	
-filename1 = "twitter-2016dev-CE-output.txt_semeval_tweets.txt"
-filename2 = "twitter-2016dev-CE-output.txt_semeval_userinfo.txt"
+filename1 = "tweets.txt"#twitter-2016dev-CE-output.txt_semeval_tweets.txt"
+filename2 = "users.txt"#"twitter-2016dev-CE-output.txt_semeval_userinfo.txt"
 preprocess(filename1, filename2)
